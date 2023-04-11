832d3afa863803fdc87296cf94b99ff9404c1bb2
Muhammad Ridjal Fathoni Rosyidi <fathonirosyidi@gmail.com>
2023-04-11 22:05:47 +0700

    refactor streamIndex.js

--- streamIndex.js
+++ streamIndex.js
@@ -1,168 +1,120 @@
-// const axios = require('axios');
-// const authData = require('./authData.json');
-
-// const twitterApi = axios.create({
-//     baseURL: 'https://api.twitter.com/2',
-//     timeout: 1000,
-//     headers: {
-//         'Content-Type': 'application/x-www-form-urlencoded',
-//         'Authorization': `Bearer ${authData.access_token}`
-//     }
-// });
-
-// setInterval(async () => {
-//     try {
-//       const userId = '1636325363897827332';
-//       const response = await twitterApi.get(`/users/${userId}/tweets`);
-//       const tweet = response.data;
-
-//       console.log(tweet);
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   }, 5000);
-
 const needle = require('needle');
-require('dotenv').config();
-const fs = require('fs');
+const dotenv = require('dotenv');
+const fs = require('fs/promises');
 
-const streamURL = `https://api.twitter.com/2/tweets/search/stream?tweet.fields=entities,created_at,public_metrics&expansions=author_id`;
+dotenv.config();
+
+const streamURL = 'https://api.twitter.com/2/tweets/search/stream?tweet.fields=entities,created_at,public_metrics&expansions=author_id';
+
 const options = {
     headers: {
-        'Authorization': `Bearer ${process.env.BEARER_TOKEN}`
-    }
+        Authorization: `Bearer ${process.env.BEARER_TOKEN}`,
+    },
 };
 
-// const stream = needle.get(streamURL, options);
-const stream = needle.get(streamURL, {
-    headers: {
-        Authorization: `Bearer ${process.env.BEARER_TOKEN}`
-    }
-})
-
+const stream = needle.get(streamURL, options);
 
 async function doRetweet(user_id, tweet_id, access_token) {
-    await needle('post', `https://api.twitter.com/2/users/${user_id}/retweets`,
-        {
-            "tweet_id": tweet_id
-        }
-        , {
+    try {
+        const res = await needle('post', `https://api.twitter.com/2/users/${user_id}/retweets`, {
+            tweet_id,
+        }, {
             headers: {
-                "Content-Type": 'application/json',
-                Authorization: `Bearer ${access_token}`
-            }
-        }).then(function (res) {
-            console.log("rt head", res?.headers)
-            console.log("rt body", res?.body)
-        }).catch(function (err) {
-            console.log('rt error', err)
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${access_token}`,
+            },
         });
+        console.log('rt head', res?.headers);
+        console.log('rt body', res?.body);
+    } catch (err) {
+        console.log('rt error', err);
+    }
 }
 
 async function doLike(user_id, tweet_id, access_token) {
-    await needle('post', `https://api.twitter.com/2/users/${user_id}/likes`,
-        {
-            tweet_id
-        },
-        {
+    try {
+        const res = await needle('post', `https://api.twitter.com/2/users/${user_id}/likes`, {
+            tweet_id,
+        }, {
             headers: {
-                "Content-Type": 'application/json',
-                Authorization: `Bearer ${access_token}`
-            }
-        }).then(function (res) {
-            console.log("like head", res?.headers)
-            console.log("like body", res?.body)
-        }).catch(function (err) {
-            console.log('like error', err)
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${access_token}`,
+            },
         });
+        console.log('like head', res?.headers);
+        console.log('like body', res?.body);
+    } catch (err) {
+        console.log('like error', err);
+    }
 }
 
 async function doComment(tweet_id, access_token, address) {
-    await needle('post', 'https://api.twitter.com/2/tweets',
-        {
+    try {
+        const res = await needle('post', 'https://api.twitter.com/2/tweets', {
             text: address,
             reply: {
-                "in_reply_to_tweet_id": tweet_id
-            }
-        },
-        {
+                in_reply_to_tweet_id: tweet_id,
+            },
+        }, {
             headers: {
                 Authorization: `Bearer ${access_token}`,
-                'Content-Type': 'application/json'
-            }
-        }).then(function (res) {
-            console.log("comment headers", res?.headers)
-            console.log("comment body", res?.body)
-        }).catch(function (err) {
-            console.log('comment error', err)
+                'Content-Type': 'application/json',
+            },
         });
+        console.log('comment headers', res?.headers);
+        console.log('comment body', res?.body);
+    } catch (err) {
+        console.log('comment error', err);
+    }
 }
 
-
-stream.on('data', async (data) => {
+async function processData(data) {
     try {
-        let authData;
-        let listAddress;
-        const datas = await new Promise((resolve, reject) => {
-            fs.readFile('./authData.json', 'utf8', (err, datas) => {
-                if (err) reject(err);
-                resolve(datas);
-            });
-        });
-        const addressDatas = await new Promise((resolve, reject) => {
-            fs.readFile('./listAddress.json', 'utf8', (err, datas) => {
-                if (err) reject(err);
-                resolve(datas);
-            });
-        });
-        listAddress = addressDatas;
-        authData = JSON.parse(datas);
+        const authData = JSON.parse(await fs.readFile('./authData.json', 'utf8'));
+        const listAddress = JSON.parse(await fs.readFile('./listAddress.json', 'utf8'));
+
         const json = JSON.parse(data);
         const { id } = json.data;
 
-        // authData.data.forEach((el, i) => {
-        //     let userTweet = listAddress[el.username];
-        //     if (userTweet) {
-        //         doRetweet(el.id, id, el.access_token);
-        //         doLike(el.id, id, el.access_token);
-        //         doComment(id, el.access_token, userTweet);
-        //     }
-        // })
+        const promises = authData.data.map(async (el) => {
+            const { id: user_id, username, access_token } = el;
+            const userTweet = listAddress[username];
 
-        const promises = authData.data.map(async (el, i) => {
-            let userTweet = listAddress[el.username];
             if (userTweet) {
-                await doRetweet(el.id, id, el.access_token);
-                await doLike(el.id, id, el.access_token);
-                await doComment(id, el.access_token, userTweet);
+                await doRetweet(user_id, id, access_token);
+                await doLike(user_id, id, access_token);
+                await doComment(id, access_token, userTweet);
+            } else {
+                console.log('gagal');
             }
         });
 
         await Promise.all(promises);
 
-        console.log(json);
+        console.log('kelar');
     } catch (error) {
-        if (data.status === 401) {
-            console.log(data);
+        if (error.status === 401) {
+            console.log(error);
             process.exit(1);
-        } else if (data.detail === "This stream is currently at the maximum allowed connection limit.") {
-            console.log(data.detail)
-            process.exit(1)
+        } else if (error.detail === 'This stream is currently at the maximum allowed connection limit.') {
+            console.log(error.detail);
+            process.exit(1);
         } else {
-            // Keep alive signal received. Do nothing.
-            console.log("error", error);
+            console.log('error', error);
         }
     }
-    // console.log('Received data:', JSON.parse(data));
-});
+}
+
+stream.on('data', processData);
 
 process.on('exit', () => {
-    stream.request.abort()
-    console.log("process.exit() method is fired on exit")
-    process.exit()
-})
+    stream.request.abort();
+    console.log('process.exit() method is fired on exit');
+    process.exit();
+});
+
 process.on('SIGINT', () => {
-    stream.request.abort()
-    console.log("process.exit() method is fired on SIGINT")
-    process.exit()
-})
+    stream.request.abort();
+    console.log('process.exit() method is fired on SIGINT');
+    process.exit();
+});
